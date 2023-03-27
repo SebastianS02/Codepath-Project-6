284e56d7353c91f08d9a8952cdefd4c5e0c1b39d
Sebastian Stec <sebastianstec@sebastians-mbp.myfiosgateway.com>
2023-03-27 14:41:13 -0400

    switched to seatgeek API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
+  const [list, setList] = useState(0);
+
+  useEffect(() => {
+    const fetchAllSeatsData = async () => {
+      const response = await fetch(
+        `https://api.seatgeek.com/2/events?client_id=${API_KEY}`
+      );
+      const json = await response.json();
+      setList(json);
+    };
+    fetchAllSeatsData().catch(console.error);
+  }, []);
+
   return (
-    <div>
-      Hello
+    <div className="whole-page">
+      <h1>Event List</h1>
     </div>
   )
 }
