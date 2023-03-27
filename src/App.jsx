72cce6e26d5152f7806b141e4c53452c80b07827
Sebastian Stec <sebastianstec@sebastians-mbp.myfiosgateway.com>
2023-03-27 13:30:55 -0400

    added API key and components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       Hello
